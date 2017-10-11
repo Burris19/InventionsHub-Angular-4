64b2cfd6d90e187129d45650cb7d009382749b41
Burris19 <julianhernandez19@gmail.com>
2017-10-10 21:57:39 -0600

    [ADD] create new component and initial with data fake

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+export class Invention {
+  name: String;
+  inventor: String;
+  year: String;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'InventionsHub!!';
+  invention: Invention = {
+    name: ' C Programing Language ',
+    inventor: ' Dennis Ritchie ',
+    year: '1972'
+  }
 }
