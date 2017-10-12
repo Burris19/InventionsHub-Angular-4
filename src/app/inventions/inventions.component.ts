f914693d8a42a221eb832350d85a590f9478d975
Burris19 <julianhernandez19@gmail.com>
2017-10-11 22:01:22 -0600

    [add] How to data binding in Angular 4 learn about 2 way data binding and displaying data via *ngFor

---
+++ src/app/inventions/inventions.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+
+export class Invention {
+    name: String;
+    inventor: String;
+    year: String;
+}
+
+@Component({
+    selector: 'app-inventions',
+    templateUrl: './inventions.component.html',
+    styleUrls: ['./inventions.component.css']
+})
+
+export class InventionsComponent implements OnInit {
+    nameModel: String;
+    inventorModel: String;
+    yearModel: String;
+    inventions: Invention[];
+
+    constructor() {
+        this.nameModel = '';
+        this.inventorModel = '';
+        this.yearModel = '';
+
+        let defaultInvention: Invention = {
+            name: 'Name Default',
+            inventor: 'Inventor Default',
+            year: 'Year Default'
+        };
+
+        this.inventions = [defaultInvention];
+    }
+
+    ngOnInit() {
+    }
+
+    createInvention() {
+
+        let newInvention: Invention = {
+            name: this.nameModel,
+            inventor: this.inventorModel,
+            year: this.yearModel
+        };
+
+        this.inventions.push(newInvention);
+
+        this.nameModel = this.yearModel = this.inventorModel = '';
+    }
+
+}
